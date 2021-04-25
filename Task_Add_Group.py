f6cc733c0369e4aa87b02d3aa7899afd45f7e6b8
Marat Sharipov <murenashark@mail.ru>
2021-04-25 17:18:09 +0300

    1 задание (рефакторинг и добавление метод.)

--- Task_Add_Group.py
+++ Task_Add_Group.py
@@ -13,19 +13,27 @@ class TaskAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-
     def test_task_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_xpath("//input[@name='user']").clear()
-        wd.find_element_by_xpath("//input[@name='user']").send_keys("admin")
-        wd.find_element_by_xpath("//input[@name='pass']").clear()
-        wd.find_element_by_xpath("//input[@name='pass']").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        # Отркытие страницы с группами
+        self.open_group_page(wd)
+        self.filling_form_group(wd)
+        #закончили заполнение
+        wd.find_element_by_link_text("group page").click()
+        #Разлогинились
+        self.logout(wd)
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def filling_form_group(self, wd):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
+        # заполнение формы
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("family")
         wd.find_element_by_name("group_header").clear()
@@ -33,10 +41,22 @@ class TaskAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("family")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        # Логаемся
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_xpath("//input[@name='user']").clear()
+        wd.find_element_by_xpath("//input[@name='user']").send_keys("admin")
+        wd.find_element_by_xpath("//input[@name='pass']").clear()
+        wd.find_element_by_xpath("//input[@name='pass']").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        # открытие главной страницы
+        wd.get("http://localhost/addressbook/group.php")
 
     def is_element_present(self, how, what):
         try:
@@ -54,8 +74,6 @@ class TaskAddGroup(unittest.TestCase):
 
     def tearDown(self):
         self.wd.quit()
-
-
 
 if __name__ == "__main__":
     unittest.main()
