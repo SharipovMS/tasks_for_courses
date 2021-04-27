9f505cf8d3b60609d30f2015ef2f5e4fb505f2f8
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-04-28 00:58:02 +0300

    3 задание внесены корректировки согласно замечаниям заказчика

---
+++ test_create_contact_class.py
@@ -0,0 +1,24 @@
+class class_for_test_create_contact:
+
+    def __init__(self, firstname, second_name, lastname, nickname, company, address, mobile, home, work,
+                           email, fax, email2, homepage, email3, bday, bmonth, byear, address2, dom, notes):
+        self.firstname = firstname
+        self.second_name = second_name
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.home = home
+        self.work = work
+        self.email = email
+        self.fax = fax
+        self.email2 = email2
+        self.homepage = homepage
+        self.email3 = email3
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.address2 = address2
+        self.dom = dom
+        self.notes = notes
