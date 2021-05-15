84727b6a9e2225a5a86f633ce68f713a182db15c
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-05-15 16:28:44 +0300

    кейс переведен на pytest подготовлена фикстура вспомогательные методы вынесены в отдельный класс application

--- add_contact_person.py
+++ add_contact_person.py
@@ -1,118 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+import pytest
 from test_create_contact_class import class_for_test_create_contact
-import unittest, time, re
+from application import Application
 
 
-class AddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Application() #инициализация фикстуры
+    request.addfinalizer(fixture.destroy) #как разрушается фикстура
+    return fixture
 
-    def add_create_contact(self):
-        self.login()
-        self.create_contact(class_for_test_create_contact(firstname="Marat", second_name="Sharipov", lastname="Sericbaevich", nickname="murena", company="quality-lab", address="Moscow",
+def add_create_contact(app): #тестовый метод
+    app.login()
+    app.create_contact(class_for_test_create_contact(firstname="Marat", second_name="Sharipov", lastname="Sericbaevich", nickname="murena", company="quality-lab", address="Moscow",
                                                               mobile="+7 906 523 43 03", home="-", work="-", email="murenashark@mail.ru", fax="-", email2="-", homepage="-", email3="-", bday="15",
                                                               bmonth="January", byear="1994", address2="Kostroma", dom="50", notes="Russia"))
-        self.logout()
+    app.logout()
 
-    def add_create_contact_empty(self):
-        self.login()
-        self.create_contact(class_for_test_create_contact(firstname="", second_name="", lastname="", nickname="", company="", address="",
+def add_create_contact_empty(app): #тестовый метод
+    app.login()
+    app.create_contact(class_for_test_create_contact(firstname="", second_name="", lastname="", nickname="", company="", address="",
                                                               mobile="", home="", work="", email="", fax="", email2="", homepage="", email3="", bday="",
                                                               bmonth="-", byear="", address2="", dom="", notes=""))
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def create_contact(self, test_create_contact_class):
-        # Заполнение формы адресной книги
-        wd = self.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(test_create_contact_class.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(test_create_contact_class.second_name)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(test_create_contact_class.lastname)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(test_create_contact_class.nickname)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(test_create_contact_class.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(test_create_contact_class.address)
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(test_create_contact_class.home)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(test_create_contact_class.mobile)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(test_create_contact_class.work)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(test_create_contact_class.fax)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(test_create_contact_class.email)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(test_create_contact_class.email2)
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(test_create_contact_class.email3)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(test_create_contact_class.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(test_create_contact_class.bday)
-        #wd.find_element_by_xpath("//option[@value='15']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(test_create_contact_class.bmonth)
-        #wd.find_element_by_xpath("//option[@value='January']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(test_create_contact_class.byear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(test_create_contact_class.address2)
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(test_create_contact_class.dom)
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(test_create_contact_class.notes)
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-
-    def login(self, login="admin", password="secret"):
-        # Авторизация
-        wd = self.wd
-        self.open_page_add_new() #вынесен в функцию чтобы страница открывалась в авторизации.
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(login)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_page_add_new(self):
-        # Открывает страницу
-        wd = self.wd
-        wd.get("https://localhost/addressbook/edit.php")
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+    app.logout()
