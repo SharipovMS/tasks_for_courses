90978240b36b2fbb9bd38c3fdd511dbd972f3fd0
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-05-21 01:51:07 +0300

    выделен вспомогательный класс для работы с группами

--- fixture/group.py
+++ fixture/group.py
@@ -5,3 +5,32 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+    def open_home_page(self):
+        # открытие главной страницы
+        wd = self.app.wd
+        wd.get("https://localhost/addressbook/")
+
+    def open_page_group(self):
+        wd = self.app.wd
+        # Открытие страницы группы
+        wd.find_element_by_link_text("groups").click()
+
+    def check_page_group(self):
+        wd = self.app.wd
+        # Проверка страницы группы
+        wd.find_element_by_link_text("groups").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        # Ввод данных в форму
+        self.open_page_group()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.check_page_group()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,10 +10,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login()
-    app.create_group(Group(name="good", header="good", footer="good"))
+    app.group.create(Group(name="good", header="good", footer="good"))
     app.session.logout()
 
 def test_add_group_empty(app):
     app.session.login()
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
