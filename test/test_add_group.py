f2f7096e2161d90a7af4e1e866cb245d605ae35e
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-05-21 00:55:08 +0300

    небольшое улучшение читаемости тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,27 +11,38 @@ class AddGroupClean(unittest.TestCase):
 
     def test_add_group_clean(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.input_login(wd, username="admin", password="secret")
         self.click_login(wd)
-        self.open_page_group(wd)
         self.create_group(wd, Group(name="good", header="good", footer="good"))
-        self.check_page_group(wd)
         self.logout(wd)
 
     def test_add_group_empty(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.input_login(wd, username="admin", password="secret")
         self.click_login(wd)
-        self.open_page_group(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.check_page_group(wd)
         self.logout(wd)
 
-    def logout(self, wd):
-        # Выход
-        wd.find_element_by_link_text("Logout").click()
+    def open_home_page(self, wd):
+        # открытие главной страницы
+        wd.get("https://localhost/addressbook/")
+
+    def click_login(self, wd):
+        # Вход
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def input_login(self, wd, username, password):
+        self.open_home_page(wd)
+        # ввод логин/пароля
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_xpath("//input[@name='user']").clear()
+        wd.find_element_by_xpath("//input[@name='user']").send_keys(username)
+        wd.find_element_by_xpath("//input[@name='pass']").clear()
+        wd.find_element_by_xpath("//input[@name='pass']").send_keys(password)
+
+    def open_page_group(self, wd):
+        # Открытие страницы группы
+        wd.find_element_by_link_text("groups").click()
 
     def check_page_group(self, wd):
         # Проверка страницы группы
@@ -39,6 +50,7 @@ class AddGroupClean(unittest.TestCase):
 
     def create_group(self, wd, group):
         # Ввод данных в форму
+        self.open_page_group(wd)
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -48,26 +60,11 @@ class AddGroupClean(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
+        self.check_page_group(wd)
 
-    def open_page_group(self, wd):
-        # Открытие страницы группы
-        wd.find_element_by_link_text("groups").click()
-
-    def click_login(self, wd):
-        # Вход
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def input_login(self, wd, username, password):
-        # ввод логин/пароля
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_xpath("//input[@name='user']").clear()
-        wd.find_element_by_xpath("//input[@name='user']").send_keys(username)
-        wd.find_element_by_xpath("//input[@name='pass']").clear()
-        wd.find_element_by_xpath("//input[@name='pass']").send_keys(password)
-
-    def open_home_page(self, wd):
-        # открытие главной страницы
-        wd.get("https://localhost/addressbook/")
+    def logout(self, wd):
+        # Выход
+        wd.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
         try:
