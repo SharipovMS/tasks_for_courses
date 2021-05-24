5955483ac6d5f2756755ad93c8bcfc9b2a759f5f
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-05-24 20:58:14 +0300

    Добавлена функция для редактирования первого контакта Исправлен тест изменения первого контакта

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -2,7 +2,7 @@ from model.test_create_contact_class import class_for_test_create_contact
 
 def test_mod_contact(app): #тестовый метод
     app.session.login()
-    app.contact.modify_first_contact(class_for_test_create_contact(firstname="Timur", second_name="", lastname="", nickname="", company="", address="",
-                                                                   mobile="", home="", work="", email="", fax="", email2="", homepage="", email3="", bday="",
-                                                                   bmonth="-", byear="", address2="", dom="", notes=""))
+    app.contact.modify_first_contact(class_for_test_create_contact(firstname="Timur", second_name="Sharipov", lastname="Sericbaevich", nickname="murena", company="quality-lab", address="Moscow",
+                                                     mobile="+7 906 523 43 03", home="-", work="-", email="murenashark@mail.ru", fax="-", email2="-", homepage="-", email3="-", bday="15",
+                                                     bmonth="January", byear="1994", address2="Kostroma", dom="50", notes="Russia"))
     app.session.logout()
