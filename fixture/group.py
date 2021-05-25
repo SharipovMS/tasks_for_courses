df97b580066ebca362933417dd4523219e2c70ad
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-05-26 00:57:38 +0300

    Оптимизация ввода данных в формы добавление теста для изменения названия группы и заголовка группы.

--- fixture/group.py
+++ fixture/group.py
@@ -20,20 +20,47 @@ class GroupHelper:
         # Проверка страницы группы
         wd.find_element_by_link_text("groups").click()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        # выбор первой группы
+        wd.find_element_by_name("selected[]").click()
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        #проверка значения
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
     def create(self, group):
         wd = self.app.wd
         # Ввод данных в форму
         self.open_page_group()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.check_page_group()
+
+    # Изменение первой группы
+    def mod_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_page_group()
+        self.select_first_group()
+        #открыть форму редактирования
+        wd.find_element_by_name("edit").click()
+        #заполненеи форм группы
+        self.fill_group_form(new_group_data)
+        #обновить данные
+        wd.find_element_by_name("update").click()
+        self.check_page_group()
+
 
     #Удаление первой группы
     def delete_first_group(self):
@@ -43,19 +70,3 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         #удалить группу
         wd.find_element_by_name("delete").click()
-
-    # Изменение первой группы
-    def mod_first_group(self, group):
-        wd = self.app.wd
-        self.open_page_group()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
-        self.check_page_group()
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,6 +1,11 @@
 from model.group import Group
 
-def test_mod_first_group(app):
+def test_mod_first_group_name(app):
     app.session.login()
-    app.group.mod_first_group(Group(name="bad", header="bad", footer="bad"))
+    app.group.mod_first_group(Group(name="bad_name"))
+    app.session.logout()
+
+def test_mod_first_group_header(app):
+    app.session.login()
+    app.group.mod_first_group(Group(header="bad_header"))
     app.session.logout()
