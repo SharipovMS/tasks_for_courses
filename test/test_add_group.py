d484c6159e8e2bfd8f2e3fde4f6c9d44f77b15b6
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-06-05 01:55:15 +0300

    реализованы проверки в виде сравнения списков для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,20 @@
 from model.group import Group
 
+
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="good", header="good", footer="good"))
+    group = Group(name="good", header="good", footer="good")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_group_empty(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group())
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
