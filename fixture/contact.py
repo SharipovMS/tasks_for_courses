07c54730483c84b017d9b153a2b6018737905455
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-06-16 11:18:59 +0300

    работа над 14 заданием, в процессе, тест падал из за тестовых данных найден деф на ui, исправил контакт, добрабатываю

--- fixture/contact.py
+++ fixture/contact.py
@@ -148,6 +148,6 @@ class ContactHelper:
         text = wd.find_element_by_id("content").text
         homephone = re.search("H: (.*)", text).group(1)
         workphone = re.search("W: (.*)", text).group(1)
-        mobilephone = re.search("M: (.*)", text).group(1)
         secondaryphone = re.search("P: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
         return Contact(homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
