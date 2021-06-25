1a3f8ac1fba2ce907ae0eaad34286d8ce33b6d4f
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-06-26 00:10:04 +0300

    21 задание

--- test/test_contact.py
+++ test/test_contact.py
@@ -1,21 +1,19 @@
+from model.contact import Contact
 import re
 
-def test_compare_contact_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
+def test_contact_db_home_page(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="Marat", lastname="TESTovich"))
+    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contact_in_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for index in range(len(db.get_contact_list())):
+        assert contact_from_home_page[index].firstname == contact_in_db[index].firstname
+        assert contact_from_home_page[index].lastname == contact_in_db[index].lastname
+        assert contact_from_home_page[index].address == contact_in_db[index].address
+        assert contact_from_home_page[index].all_phones_from_home_page == merge_phones_like_on_home_page(contact_in_db[index])
+        assert contact_from_home_page[index].all_emails_from_home_page == merge_emails_like_on_home_page(contact_in_db[index])
 
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
-    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
+
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -24,10 +22,9 @@ def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                        [contact.homephone, contact.workphone, contact.mobilephone, contact.secondaryphone]))))
+                                       [contact.homephone, contact.workphone, contact.mobilephone, contact.secondaryphone]))))
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3]))))
+                     filter(lambda x: x is not None,
+                            [contact.email, contact.email2, contact.email3])))
