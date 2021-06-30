40ab8baa99a0b31a2ee6d1a13d3ac344e15e4629
Marat Sharipov <83124202+SharipovMS@users.noreply.github.com>
2021-06-30 11:44:48 +0300

    правки 20

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -1,9 +1,10 @@
 from model.contact import Contact
 from random import randrange
+from time import sleep
 
 def test_mod_contact(app, db, check_ui): #тестовый метод
     if len(db.get_contact_list()) == 0:
-        app.contact.create((Contact(firstname="Marat", lastname="TESTovich")))
+        app.contact.create(Contact(firstname="Marat", lastname="TESTovich"))
     old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     contact = Contact(firstname="Tamerlan")
@@ -11,10 +12,11 @@ def test_mod_contact(app, db, check_ui): #тестовый метод
     app.contact.modify_contact_by_id(contact.id, contact)
     new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    sleep(2)
     assert old_contacts == new_contacts
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_group_list(), key=Contact.id_or_max())
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
 #def test_mod_contact(app, db, check_ui): #тестовый метод
 #    if app.contact.count() == 0:
